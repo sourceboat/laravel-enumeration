242c4c9765297d2b8f9689bff715d91333603393
Kevin Buchholz <kevin@sourceboat.com>
2019-10-03 17:59:22 +0200

    add tests for between comparison

---
+++ tests/unit/Weighted/IsBetweenOrEqualToTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sourceboat\Enumeration\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class IsBetweenOrEqualToTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setup();
+
+        config([
+            'enums' => [
+                FruitType::class => [
+                    'weights' => [
+                        FruitType::NUT => 1,
+                        FruitType::BERRY => 2,
+                        FruitType::LEGUME => 3,
+                        FruitType::ACCESSORY_FRUIT => 4,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * Data provider for the test `testIsBetweenOrEqualTo`.
+     *
+     * @return array
+     */
+    public function dataProvider(): array
+    {
+        return [
+            [ FruitType::BERRY(), FruitType::ACCESSORY_FRUIT(), FruitType::NUT(), false ],
+            [ FruitType::BERRY(), FruitType::ACCESSORY_FRUIT(), FruitType::BERRY(), true ],
+            [ FruitType::BERRY(), FruitType::ACCESSORY_FRUIT(), FruitType::LEGUME(), true ],
+            [ FruitType::BERRY(), FruitType::ACCESSORY_FRUIT(), FruitType::ACCESSORY_FRUIT(), true ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @param \Sourceboat\Enumeration\Tests\FruitType $lower
+     * @param \Sourceboat\Enumeration\Tests\FruitType $higher
+     * @param \Sourceboat\Enumeration\Tests\FruitType $needle
+     * @param bool $result
+     * @return void
+     */
+    public function testIsBetweenOrEqualTo(FruitType $lower, FruitType $higher, FruitType $needle, bool $result): void
+    {
+        $this->assertEquals($result, $needle->isBetweenOrEqualTo($lower, $higher));
+    }
+}
