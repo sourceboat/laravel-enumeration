d253563fb42f3ae6a99e3b2e3a711c53e013fb60
Kevin Buchholz <kevin@sourceboat.com>
2020-03-26 22:12:05 +0100

    adding laravel 7 custom cast feature

---
+++ src/Casts/Enum.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Sourceboat\Enumeration\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class Enum implements CastsAttributes
+{
+    private $enumClass;
+
+    private $nullable;
+
+    public function __construct(string $enumClass, bool $nullable = true)
+    {
+        $this->enumClass = $enumClass;
+        $this->nullable = $nullable;
+    }
+
+    /**
+     * Transform the attribute from the underlying model values.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array<mixed>  $attributes
+     * @return self|null
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ReturnTypeHint.MissingNativeTypeHint
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        if ($this->enumClass::hasMemberWithValueOf($value)) {
+            return $this->enumClass::memberByValue($value);
+        }
+
+        if ($this->nullable && is_null($value)) {
+            return $value;
+        }
+
+        return $this->enumClass::defaultMember();
+    }
+
+    /**
+     * Transform the attribute to its underlying model values.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array<mixed>  $attributes
+     * @return array<mixed>
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function set($model, string $key, $value, array $attributes): array
+    {
+        if ($this->enumClass::hasMemberWithValueOf($value)
+            || ($this->nullable && is_null($value))) {
+            return [
+                $key => $value,
+            ];
+        }
+
+        if ($value instanceof $this->enumClass) {
+            return [
+                $key => $value->value(),
+            ];
+        }
+
+        return [
+            $key => $this->enumClass::defaultMember(),
+        ];
+    }
+}
